ec27df5c11ea6219bda62c9a8019b4e187b7a0bb
clisdermar <clisdermar@gmail.com>
2024-01-19 17:52:04 +0000

    generador de escusa

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomainNames() {
+  let domainNames = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domainName = pronoun[i] + adj[j] + noun[k] + '.com';
+        domainNames.push(domainName);
+      }
+    }
+  }
+
+  return domainNames;
+}
+
+let result = generateDomainNames();
+console.log(result);
