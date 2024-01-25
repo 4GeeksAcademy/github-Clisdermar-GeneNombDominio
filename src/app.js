2a7fa0779f8b89219262d994ff1bb1061172a638
clisdermar <clisdermar@gmail.com>
2024-01-25 02:59:56 +0000

    Generador de dominio y hack

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
+let noun = ["jogger", "racoon", "caminacom"];
+let extension = [".com", ".es"];
 
 function generateDomainNames() {
   let domainNames = [];
@@ -8,8 +9,19 @@ function generateDomainNames() {
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        let domainName = pronoun[i] + adj[j] + noun[k] + ".com";
-        domainNames.push(domainName);
+        for (let m = 0; m < extension.length; m++) {
+          let domainName;
+
+          let extensionMatch = extension[m].replace(".", "");
+          if (noun[k].endsWith(extensionMatch)) {
+            let modifiedNoun = noun[k].slice(0, -extensionMatch.length);
+            domainName = pronoun[i] + adj[j] + modifiedNoun + extension[m];
+          } else {
+            domainName = pronoun[i] + adj[j] + noun[k] + extension[m];
+          }
+
+          domainNames.push(domainName);
+        }
       }
     }
   }
@@ -19,7 +31,6 @@ function generateDomainNames() {
 
 let result = generateDomainNames();
 
-// Imprimir solo los valores
-result.forEach(function(valores) {
-  console.log(valores);
+result.forEach(function(valores, index) {
+  console.log(valores, index);
 });
